5e78df767f7b7e51b15640de92a6dc3067cae6f7
Felipe Ignacio Acuña Gonzalez (LATAM) <felipe.acuna@latam.com>
2024-03-21 20:15:51 -0300

    next auth added but without env

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import Navbar from "@/components/Navbar";
+import {Providers} from './Providers'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,12 +13,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          <Navbar />
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
