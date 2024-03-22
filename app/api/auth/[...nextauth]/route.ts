f56fba7970757802eae99a5f8b15f2944bd6798e
Felipe Ignacio Acuña Gonzalez (LATAM) <felipe.acuna@latam.com>
2024-03-21 21:24:53 -0300

    fixes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -8,6 +8,23 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  callbacks: {
+    async jwt({token, user, account, profile}) {
+      if (account) {
+        token.id = user.id;
+        token.name = user.name;
+        token.email = user.email;
+        token.image = user.image;
+        token.id_token = account.id_token;
+        token.prueba = account;
+      }
+      return token;
+    },
+    async session({ session, token }: { session: any, token: any }) {
+      session.user = token;
+      return session;
+    }
+  }
 });
 
 export { handler as GET, handler as POST };
